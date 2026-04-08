0abd3e054f97f5a0e64f01a112d355ac6f2c1678
Yatin Singh <yatinsingh331@gmail.com>
2026-04-08 22:31:39 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ app.get('/health', (req, res) => {
 app.use('/api', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
